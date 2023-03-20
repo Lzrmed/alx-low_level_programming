33549b031bf25e793efede2a297ead6594041b70
lazar <mohammedlazar007@gmail.com>
2023-03-20 16:45:28 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
-int main(void)
+/*
+* File: 7-print_tebahpla.c
+* Auth: LAZ_MED
+*/
 
+#include <stdio.h>
+
+/**
+ * main - Prints
+ *
+ * Return: Alway 0 (Success)
+ */
+int main(void)
 {
 	char l;
 
 	for (l = 'z'; l >= 'a'; l--)
-
 	{
 		putchar(l);
 	}
