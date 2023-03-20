26da90841879104b0b8ed18232a4aa1519a44681
lazar <mohammedlazar007@gmail.com>
2023-03-20 16:40:57 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,25 @@
+/*
+ * file: 5-print_numbers.c
+ * Auth: LZR_med
+ */
+
 #include <stdio.h>
+
 /**
-* main - Prints
+* main - PrintS from 0 to 9
 *
-* Return: 0 (Success)
+* Return: Always 0 (Success)
 */
 int main(void)
 {
 	int i;
 
-	for (i = 0;i<= 9;i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
-		printf("%d",i);
+		printf("%d", i);
 	}
+
 	printf("\n");
+
 	return (0);
 }
